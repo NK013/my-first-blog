ed0c90bad5e9eec7b151bfb36bcd307fd889abfb
NK013 <noah.kipp@berlin.de>
2024-07-11 14:29:43 +0200

    Removed post_list(*) & Added Shoppinglist(*)

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MinLengthValidator
+from django.core.validators import MaxLengthValidator
 
 
 class Post(models.Model):
@@ -16,3 +18,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Item(models.Model):
+    itemname = models.CharField(
+        validators=[
+            MinLengthValidator(4, 'Der Name kann nur 4 - 24 Zeichen enthalten!')
+            ], max_length=24
+        )
+    number = models.CharField(
+        validators=[
+            MinLengthValidator(1, 'Die Anzahl kann nur nur ein- bis dreistellig sein!!')
+            ], max_length=3
+        )
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from blog.models import Item
 
-def post_list(request):
-    return render(request, 'blog/index.html', {})
+#def post_list(request):
+#    return render(request, 'blog/index.html', {})
+                  
+def shoppinglist(request):
+    return render(request, 'blog/shoppinglist.html', {'shoppinglist': Item.objects.all()})
